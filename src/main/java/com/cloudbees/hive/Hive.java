7f61ab052ae79db13a6d06d56f1532a5b2b5acd6
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2017-02-04 01:20:10 +0100

    Setup application entrypoint

---
+++ src/main/java/com/cloudbees/hive/Hive.java
@@ -0,0 +1,15 @@
+package com.cloudbees.hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Adrien Lecharpentier
+ */
+@SpringBootApplication
+public class Hive {
+    public static void main(String[] args) {
+        SpringApplication.run(Hive.class, args);
+    }
+}
+
