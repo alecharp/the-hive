a413287d0e50621374c41bec334775b984a9de27
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2017-02-13 20:50:41 +0100

    Make sure location stays on Earth

--- src/main/java/com/cloudbees/hive/model/Location.java
+++ src/main/java/com/cloudbees/hive/model/Location.java
@@ -12,6 +12,12 @@ public class Location {
     }
 
     public Location(double latitude, double longitude) {
+        if (latitude < -90 || latitude > 90) {
+            throw new IllegalArgumentException("Latitude must be between -90 and 90");
+        }
+        if(longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException("Longitude must be between -180 and 180");
+        }
         this.longitude = longitude;
         this.latitude = latitude;
     }
