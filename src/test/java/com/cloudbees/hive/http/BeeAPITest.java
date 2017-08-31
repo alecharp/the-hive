7e0efd4dd747dc03bf1da0537d43dd1a73110aa6
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2017-08-31 19:54:24 +0200

    Fix tests configuration

--- src/test/java/com/cloudbees/hive/http/BeeAPITest.java
+++ src/test/java/com/cloudbees/hive/http/BeeAPITest.java
@@ -3,6 +3,7 @@ package com.cloudbees.hive.http;
 import com.cloudbees.hive.model.Bee;
 import com.cloudbees.hive.service.BeeService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,10 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static java.util.Optional.empty;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -31,15 +37,21 @@ public class BeeAPITest {
     @Autowired private MockMvc mvc;
     @MockBean private BeeService beeService;
 
+    @Autowired private Gson gson;
+    @Autowired private ObjectMapper objectMapper;
+
     @Test
-    public void shouldBeAbleToGetTheHiveMember() throws Exception {
+    public void shouldBeAbleToGetTheHiveMembers() throws Exception {
         List<Bee> hive = Arrays.asList(
             new Bee("Adrien L.", "Paris, France"),
             new Bee("Arnaud H.", "Paris, France")
         );
+        given(this.beeService.all()).willReturn(hive);
 
-        given(this.beeService.all())
-            .willReturn(hive);
+        Field id = Bee.class.getDeclaredField("id");
+        id.setAccessible(true);
+        id.set(hive.get(0), UUID.randomUUID().toString());
+        id.set(hive.get(1), UUID.randomUUID().toString());
 
         this.mvc.perform(get("/api/hive"))
             .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
@@ -51,16 +63,42 @@ public class BeeAPITest {
     }
 
     @Test
+    public void shouldBeAbleToRetrieveOneBee() throws Exception {
+        Bee maya = new Bee("John D.", "New York City, US");
+        given(this.beeService.byId(anyString())).willReturn(Optional.of(maya));
+
+        this.mvc.perform(get("/api/bee/foobar"))
+            .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().json(gson.toJson(maya)));
+    }
+
+    @Test
+    public void shouldNotBeAbleToRetrieveNonexistentBees() throws Exception {
+        given(this.beeService.byId(anyString())).willReturn(empty());
+
+        this.mvc.perform(get("/api/bee/foobar"))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
     public void shouldBeAbleToCreateANewBee() throws Exception {
         Bee maya = new Bee("John D.", "New York City, US");
         given(this.beeService.add(maya)).willReturn(maya);
 
-        ObjectMapper objectMapper = new ObjectMapper();
+        Field id = maya.getClass().getDeclaredField("id");
+        id.setAccessible(true);
+        id.set(maya, UUID.randomUUID().toString());
 
         this.mvc.perform(post("/api/bee")
             .contentType(MediaType.APPLICATION_JSON)
             .content(objectMapper.writeValueAsBytes(maya)))
             .andExpect(status().isCreated())
-            .andExpect(header().string("Location", Matchers.endsWith(maya.getId())));
+            .andExpect(header().string("Location",
+                Matchers.allOf(
+                    Matchers.startsWith("/api/bee/"),
+                    Matchers.endsWith(maya.getId())
+                )
+            ));
     }
 }
--- src/test/java/com/cloudbees/hive/service/BeeServiceIT.java
+++ src/test/java/com/cloudbees/hive/service/BeeServiceIT.java
@@ -1,38 +1,37 @@
 package com.cloudbees.hive.service;
 
 import com.cloudbees.hive.model.Bee;
-import com.cloudbees.hive.repository.BeeRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
+import static org.springframework.boot.autoconfigure.jdbc.EmbeddedDatabaseConnection.H2;
 
 /**
  * @author Adrien Lecharpentier
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class BeeServiceReadTest {
-
-    @MockBean private BeeRepository beeRepository;
+@DataJpaTest
+@AutoConfigureTestDatabase(connection = H2)
+@Import(BeeService.class)
+public class BeeServiceIT {
     @Autowired private BeeService beeService;
 
     @Test
-    public void shouldBeAbleToGetTheHive() {
+    public void shouldBeAbleToSaveBeesInsideTheHive() {
         List<Bee> expectedHive = Arrays.asList(
             new Bee("Adrien L.", "Paris, France"),
             new Bee("Arnaud H.", "Paris, France")
         );
-
-        given(this.beeRepository.findAll()).willReturn(expectedHive);
+        expectedHive.forEach(beeService::add);
         assertThat(this.beeService.all()).containsExactlyElementsOf(expectedHive);
     }
 }
