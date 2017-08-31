5ac1f0a994b4b693d7ed039863be71e01453824a
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2017-08-31 19:54:24 +0200

    Fix tests configuration

--- src/test/java/com/cloudbees/hive/HiveIT.java
+++ src/test/java/com/cloudbees/hive/HiveIT.java
@@ -1,33 +1,29 @@
 package com.cloudbees.hive;
 
-import com.cloudbees.hive.model.Bee;
-import com.cloudbees.hive.repository.BeeRepository;
+import com.cloudbees.hive.service.BeeService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.htmlunit.webdriver.MockMvcHtmlUnitDriverBuilder;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
 
 /**
  * @author Adrien Lecharpentier
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest
 public class HiveIT {
 
     @Autowired private WebApplicationContext context;
     private WebDriver webDriver;
-    @MockBean private BeeRepository beeRepository;
+    @MockBean private BeeService beeService;
 
     @Before
     public void setup() {
@@ -36,13 +32,6 @@ public class HiveIT {
             .useMockMvcForHosts("localhost")
             .javascriptEnabled(true)
             .build();
-
-        when(beeRepository.findAll()).thenReturn(
-            Arrays.asList(
-                new Bee("John D.", "j@f.us", "New York City, US"),
-                new Bee("M. X", "m@x.fr", "Paris, France")
-            )
-        );
     }
 
     @Test
