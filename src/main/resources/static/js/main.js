d4bd78b47ceae826837db0406298e8eca7db18bb
Carlos Rodriguez Lopez <it.carlosrodlop@gmail.com>
2017-09-14 12:22:25 +0200

    Integrate google autocomplete

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,6 +12,11 @@
       .bindPopup(`<b>${bee.name}</b>`)
       .addTo(hive);
   };
+
+  const zoom2Bee = function (bee) {
+    hive.flyTo(L.latLng(bee.latitude, bee.longitude),5);
+  };
+
   $.get('/api/hive', function(bees) {
     bees.forEach(addBeeToMap);
   });
@@ -28,6 +33,7 @@
   $('button#create-bee').click(function(event) {
     event.preventDefault();
     modal.modal('show');
+    geoCoding();
   });
 
   /**
@@ -39,6 +45,15 @@
     const [minValue, maxValue] = boundaries.split(':');
     return Number(value) >= Number(minValue) && Number(value) <= Number(maxValue);
   };
+
+  /**
+   * Verify that one of the options from the suggested locations on the dropdowm menu has been selecting input before submitting the form
+   *
+   */
+  var geometry;
+  $.fn.form.settings.rules.geometry = function() {
+    return geometry;
+  }
 
   const form = $('.ui.form');
   form.form({
@@ -57,7 +72,7 @@
           form.form('reset');
           modal.modal('hide');
           addBeeToMap(bee);
-          hive.panTo([fields.latitude, fields.longitude]);
+          zoom2Bee(bee);
         })
         .fail(function () {
           form.form('set error');
@@ -105,9 +120,19 @@
           type: 'boundaries[-180:180]',
           prompt: 'The latitude must be higher than -180 and less than 180'
         }]
+      },
+      place_input: {
+        rules: [{
+          type: 'empty',
+          prompt: 'Place cannot be empty'
+        }, {
+          type: 'geometry',
+          prompt: 'A place must be selected from the suggestions on the dropdown list'
+        }]
       }
     }
   });
+
   $('button#validate-form').click(function(event) {
     event.preventDefault();
     form.form('validate form');
@@ -132,14 +157,40 @@
     })
   });
 
-  const geocodingButton = $('.ui.button#geocoding');
-  geocodingButton.click(function(event) {
-    event.preventDefault();
-    // TODO
-
-    form.form('set values', {
-      latitude: -90,
-      longitude: -180
-     });
-  });
+  const geocodingButton = $('.ui.button#geocoding'); // it is not used
+  const card = $('#pac-card');
+  const input = document.getElementById('pac-input');
+  //const input = $('.ui.input#pac-input');
+  const infowindowContent = document.getElementById('infowindow-content');
+  //const infowindowContent = $('#infowindow-content');
+  const geoCoding = function() {
+    geometry=false;
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    var infowindow = new google.maps.InfoWindow();
+    infowindow.setContent(infowindowContent);
+    autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+      if (place.geometry) {
+        geometry=true;
+        var place_output_str = '';
+        if (place.address_components) {
+          //TODO place: just select political types: country - administrative_area (level 1 and 2). Not streets and numbers
+          place_output_str = [
+            (place.address_components[0] && place.address_components[0].long_name || ''),
+            (place.address_components[1] && place.address_components[1].long_name || ''),
+            (place.address_components[2] && place.address_components[2].long_name || '')
+            ].join(' ');
+        }
+        console.log("place_output :" + place_output_str)
+        // Populating field
+        form.form('set values', {
+        latitude: place.geometry.location.lat(),
+        longitude: place.geometry.location.lng(),
+        //TODO place_output_str: >>> Bee.place
+        place_output: place_output_str
+        });
+      }
+    });
+  }
 })();
